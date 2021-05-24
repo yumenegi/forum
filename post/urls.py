1ef21acdc280359a269c77c757bd9e333b90d855
Hausen <70286690+hausen-wu-23@users.noreply.github.com>
2021-05-24 11:40:15 -0700

    Thread label finished, main site change to /all/

--- post/urls.py
+++ post/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('posts/', views.MainPage.as_view()),
+    path('all/', views.MainPage.as_view()),
     path('threads/', views.Threads.as_view()),
     path('posts/<int:post_id>/', views.detail, name='detail'),
-    # path('threads/<str:thread_id>/', views.ThreadView.as_view(), name='threadview'),
+    path('threads/<str:thread_id>/', views.ThreadView.as_view(), name='threadview'),
     path('new/', views.NewPost.as_view(), name="new_post"),
     path('login/', views.LogIn.as_view(), name='login'),
 ]
--- post/views.py
+++ post/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render
 from django.template import loader
-from django.shortcuts import get_object_or_404, render,HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render, HttpResponseRedirect
 from django.contrib.auth.models import User, AnonymousUser
 from django.contrib.auth import authenticate, login, logout
 from django.views import View
@@ -17,7 +17,7 @@ from .models import Post
 
 def index(request):
     # temporary landing page
-    return HttpResponse('<a href="/posts/">Welcome!</a>')
+    return HttpResponse('<a href="/all/">Welcome!</a>')
 
 
 def detail(request, post_id):
@@ -55,32 +55,32 @@ def detail(request, post_id):
 #         }
 #         return render(request, 'post/newPost.html', context)
 
+
 class LogIn(View):
     def get(self, request):
-        context={}
+        context = {}
         return render(request, "post/logIn.html", context)
 
     def post(self, request):
         # if request.POST.has_key("username"):
         username = request.POST["username"]
         password = request.POST["password"]
-        user = authenticate(request, username = username, password = password)
+        user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return HttpResponse('logged in as ' + str(user))
-            
-            
-        return HttpResponse('invalid')
+            return HttpResponse("logged in as " + str(user))
+
+        return HttpResponse("invalid")
+
 
 class NewPost(View):
     def get(self, request):
         if request.user.is_anonymous:
             return HttpResponse("Please Sign In")
-        
+
         form = SubmitPost()
         context = {"form": form}
         return render(request, "post/newPost.html", context)
-
 
     def post(self, request):
         form = request.POST
@@ -109,75 +109,75 @@ class NewPost(View):
 
         # return HttpResponse('invalid')
 
+
 class MainPage(View):
     def post(self, request):
-        if 'logout' in request.POST.keys():
+        if "logout" in request.POST.keys():
             logout(request)
         else:
             username = request.POST["username"]
             password = request.POST["password"]
-            user = authenticate(request, username = username, password = password)
+            user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
 
         # post_objects = Post.objects.order_by("pub_date")
         # context = {"post_objects": post_objects}
-        
+
         # return render(request, "post/index.html", context)
         return HttpResponseRedirect(request.path)
-            
 
     def get(self, request):
         post_objects = Post.objects.order_by("-pub_date")
         context = {"post_objects": post_objects}
         return render(request, "post/index.html", context)
 
+
 class Threads(View):
     def post(self, request):
-        if 'logout' in request.POST.keys():
+        if "logout" in request.POST.keys():
             logout(request)
         else:
             username = request.POST["username"]
             password = request.POST["password"]
-            user = authenticate(request, username = username, password = password)
+            user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
 
         # post_objects = Post.objects.order_by("pub_date")
         # context = {"post_objects": post_objects}
-        
+
         # return render(request, "post/index.html", context)
         return HttpResponseRedirect(request.path)
-            
 
     def get(self, request):
         thread_object = Thread.objects.all()
         context = {"thread_object": thread_object}
         return render(request, "post/thread.html", context)
 
-# class ThreadView(View):
-#     def post(self, request, thread_id):
-#         if 'logout' in request.POST.keys():
-#             logout(request)
-#         else:
-#             username = request.POST["username"]
-#             password = request.POST["password"]
-#             user = authenticate(request, username = username, password = password)
-#             if user is not None:
-#                 login(request, user)
 
-#         # post_objects = Post.objects.order_by("pub_date")
-#         # context = {"post_objects": post_objects}
-        
-#         # return render(request, "post/index.html", context)
-#         return HttpResponseRedirect(request.path)
-            
+class ThreadView(View):
+    def post(self, request, thread_id):
+        if "logout" in request.POST.keys():
+            logout(request)
+        else:
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
 
-#     def get(self, request, thread_id):
-#         # print(thread_id)
-#         request_thread = get_object_or_404(Thread, pk=thread_id)
-#         # print(type(request_thread))
-#         post_objects = Post.objects.filter(thread=request_thread)
-#         print(post_objects)
-#         context = {"post_objects": post_objects}
-#         return render(request, "post/index.html", context)
+        # post_objects = Post.objects.order_by("pub_date")
+        # context = {"post_objects": post_objects}
+
+        # return render(request, "post/index.html", context)
+        return HttpResponseRedirect(request.path)
+
+    def get(self, request, thread_id):
+        # print(thread_id)
+        request_thread = get_object_or_404(Thread, pk=thread_id)
+        # print(type(request_thread))
+        post_objects = Post.objects.filter(thread=request_thread).order_by("pub_date")
+        print(post_objects)
+        context = {"post_objects": post_objects, "title": request_thread.title}
+        return render(request, "post/threadList.html", context)
