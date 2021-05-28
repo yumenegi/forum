3b04d1d3b6b0c697a029a30fa3731aa7b235bc41
Hausen <70286690+hausen-wu-23@users.noreply.github.com>
2021-05-28 12:57:52 -0700

    Deletion functionality fully implemented!

--- post/urls.py
+++ post/urls.py
@@ -7,10 +7,11 @@ urlpatterns = [
     path('all/', views.MainPage.as_view()),
     path('threads/', views.Threads.as_view()),
     path('posts/<int:post_id>/', views.detail, name='detail'),
+    path('posts/<int:post_id>/delete', views.Delete.as_view(), name='delete'),
     path('threads/<str:thread_id>/', views.ThreadView.as_view(), name='threadview'),
     path('new/', views.NewPost.as_view(), name="new_post"),
     path('new/thread', views.NewThread.as_view(), name="new_post"),
     path('login/', views.LogIn.as_view(), name='login'),
     path('register/', views.Register.as_view(), name='register'),
-    path('myprofile/', views.MyProfile.as_view(), name='myprofile')
+    path('profile/<int:user_id>', views.Profile.as_view(), name='profile')
 ]
--- post/views.py
+++ post/views.py
@@ -1,3 +1,4 @@
+from django.http.request import HttpRequest
 from django.shortcuts import redirect, render
 from django.template import loader
 from django.shortcuts import get_object_or_404, render, HttpResponseRedirect
@@ -17,14 +18,14 @@ from .models import Post
 
 def index(request):
     # temporary landing page
-    return render(request, "post/landing.html")
+    return render(request, 'post/landing.html')
 
 
 def detail(request, post_id):
     # shows content of the post
     post = get_object_or_404(Post, pk=post_id)
-    context = {"post": post}
-    return render(request, "post/detail.html", context)
+    context = {'post': post}
+    return render(request, 'post/detail.html', context)
 
 
 # def new_post(request):
@@ -33,7 +34,7 @@ def detail(request, post_id):
 
 #     # if there is data coming in, normally GET, but when data, POST
 #     if request.method == 'POST':
-#         # print("POST!")
+#         # print('POST!')
 #         # print(request.POST)
 
 #         # create the model form the QueryDict
@@ -59,127 +60,127 @@ def detail(request, post_id):
 class LogIn(View):
     def get(self, request):
         context = {}
-        return render(request, "post/logIn.html", context)
+        return render(request, 'post/logIn.html', context)
 
     def post(self, request):
-        # if request.POST.has_key("username"):
-        username = request.POST["username"]
-        password = request.POST["password"]
+        # if request.POST.has_key('username'):
+        username = request.POST['username']
+        password = request.POST['password']
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return HttpResponseRedirect("/all/")
+            return HttpResponseRedirect('/all/')
 
-        return HttpResponse("invalid")
+        return HttpResponse('invalid')
 
 
 class NewPost(View):
     def get(self, request):
         if request.user.is_anonymous:
-            return HttpResponse("Please Sign In")
+            return HttpResponse('Please Sign In')
 
         form = SubmitPost()
-        context = {"form": form}
-        return render(request, "post/newPost.html", context)
+        context = {'form': form}
+        return render(request, 'post/newPost.html', context)
 
     def post(self, request):
         form = request.POST
         # print(thre)
         save_data = Post.objects.create(
             userPosted=request.user,
-            thread=Thread.objects.get(pk=form["thread"]),
-            title=form["title"],
-            content=form["content"],
+            thread=get_object_or_404(Thread, pk=form['thread']),
+            title=form['title'],
+            content=form['content'],
             pub_date=datetime.datetime.now(),
         )
         save_data.save()
-        return HttpResponseRedirect("/posts/" + str(save_data.id))
+        return HttpResponseRedirect('/posts/' + str(save_data.id))
 
 
 class NewThread(View):
     def get(self, request):
         if request.user.is_anonymous:
-            return HttpResponse("Please Sign In")
-        return render(request, "post/newForum.html")
+            return HttpResponse('Please Sign In')
+        return render(request, 'post/newForum.html')
 
     def post(self, request):
         form = request.POST
-        save_data = Thread.objects.create(title=form["title"])
+        save_data = Thread.objects.create(title=form['title'])
         save_data.save()
-        return HttpResponseRedirect("/threads/" + str(save_data.id))
+        return HttpResponseRedirect('/threads/' + str(save_data.id))
 
 
 class MainPage(View):
     def post(self, request):
-        if "logout" in request.POST.keys():
+        if 'logout' in request.POST.keys():
             logout(request)
         else:
-            username = request.POST["username"]
-            password = request.POST["password"]
+            username = request.POST['username']
+            password = request.POST['password']
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
 
-        # post_objects = Post.objects.order_by("pub_date")
-        # context = {"post_objects": post_objects}
+        # post_objects = Post.objects.order_by('pub_date')
+        # context = {'post_objects': post_objects}
 
-        # return render(request, "post/index.html", context)
+        # return render(request, 'post/index.html', context)
         return HttpResponseRedirect(request.path)
 
     def get(self, request):
-        post_objects = Post.objects.order_by("-pub_date")
-        context = {"post_objects": post_objects}
-        return render(request, "post/index.html", context)
+        post_objects = Post.objects.order_by('-pub_date')
+        context = {'post_objects': post_objects}
+        return render(request, 'post/index.html', context)
 
 
 class Threads(View):
     def post(self, request):
-        if "logout" in request.POST.keys():
+        if 'logout' in request.POST.keys():
             logout(request)
         else:
-            username = request.POST["username"]
-            password = request.POST["password"]
+            username = request.POST['username']
+            password = request.POST['password']
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
 
-        # post_objects = Post.objects.order_by("pub_date")
-        # context = {"post_objects": post_objects}
+        # post_objects = Post.objects.order_by('pub_date')
+        # context = {'post_objects': post_objects}
 
-        # return render(request, "post/index.html", context)
+        # return render(request, 'post/index.html', context)
         return HttpResponseRedirect(request.path)
 
     def get(self, request):
         thread_object = Thread.objects.all()
-        context = {"thread_object": thread_object}
-        return render(request, "post/thread.html", context)
+        context = {'thread_object': thread_object}
+        return render(request, 'post/thread.html', context)
 
 
 class ThreadView(View):
-    def post(self, request):
-        if "logout" in request.POST.keys():
+    def post(self, request, thread_id):
+        if 'logout' in request.POST.keys():
             logout(request)
         else:
-            username = request.POST["username"]
-            password = request.POST["password"]
+            username = request.POST['username']
+            password = request.POST['password']
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
 
-        # post_objects = Post.objects.order_by("pub_date")
-        # context = {"post_objects": post_objects}
+        # post_objects = Post.objects.order_by('pub_date')
+        # context = {'post_objects': post_objects}
 
-        # return render(request, "post/index.html", context)
+        # return render(request, 'post/index.html', context)
         return HttpResponseRedirect(request.path)
 
     def get(self, request, thread_id):
         # print(thread_id)
         request_thread = get_object_or_404(Thread, pk=thread_id)
         # print(type(request_thread))
-        post_objects = Post.objects.filter(thread=request_thread).order_by("pub_date")
+        post_objects = Post.objects.filter(thread=request_thread).order_by('-pub_date')
         print(post_objects)
-        context = {"post_objects": post_objects, "title": request_thread.title}
-        return render(request, "post/threadList.html", context)
+        context = {'post_objects': post_objects, 'title': request_thread.title}
+        return render(request, 'post/threadList.html', context)
 
 
 class Register(View):
@@ -194,19 +195,44 @@ class Register(View):
 
     def get(self, request):
         form = RegisterForm()
-        context = {"form":form}
+        context = {'form':form}
         return render(request, 'post/register.html', context)
 
-class MyProfile(View):
-    def get(self, request):
-        if not request.user.is_anonymous:
-            post = Post.objects.filter(user=request.user)
-            context = {'post', post}
-            return render(request, 'post/profile.html', context)
-        
+class Profile(View):
+    def post(self, request, user_id):
+        if 'logout' in request.POST.keys():
+            logout(request)
         else:
-            return HttpResponse("Please sign in.")
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
 
+        return HttpResponseRedirect(request.path)
+
+    def get(self, request, user_id):
+        user = get_object_or_404(User, pk=user_id)
+        post = Post.objects.filter(userPosted=user).order_by('-pub_date')
+        is_self = user == request.user
+        print(is_self)
+        context = {'post_objects': post, 'is_self':is_self}
+        return render(request, 'post/profile.html', context)
+        
+class Delete(View):
+    def post(self, request, post_id):
+        delete_post = get_object_or_404(Post, pk=post_id)
+        if 'delete' in request.POST.keys() and request.user == delete_post.userPosted:
+            delete_post.delete()
+            return HttpResponseRedirect('/profile/' + str(request.user.id))
+        else:
+            return HttpResponseRedirect('/profile/' + str(request.user.id))
+
+    def get(self, request, post_id):
+        delete_post = get_object_or_404(Post, pk=post_id)
+        context = {'post':delete_post}
+        return render(request, 'post/delete.html', context)
+        
 # user regist
 # deletion
 # styling
